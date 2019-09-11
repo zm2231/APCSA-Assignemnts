b2fe3b9a6ad056d5fd10918ffada4597a6641188
Zain Merchant <merchantzains@gmail.com>
2019-09-11 11:18:52 -0400

    Update CalculateCircle.java

--- CalculateCircle.java
+++ CalculateCircle.java
@@ -19,5 +19,14 @@ class CalculateCircle
       //Circumference = 2*PI*radius
       double circumference= Math.PI * 2*radius;
       System.out.println( "The circumference of the circle is:"+circumference) ;
+      radius = radius *2;
+        System.out.println("double the radius is" + radius);
+        //Area = PI*radius*radius
+      double area2 = Math.PI * (radius * radius);
+      System.out.println("The area of circle with double the radius is: " + area2);
+      //Circumference = 2*PI*radius
+      double circumference2= Math.PI * 2*radius;
+      System.out.println( "The circumference of the circle with double the radius is:"+circumference2) ;
+        
    }
 }
